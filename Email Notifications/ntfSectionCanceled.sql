98bbb10514f008bce9448a342880d90161a0ac6d
bendodge-xwis <32881391+bendodge-xwis@users.noreply.github.com>
2020-04-27 15:40:05 -0600

    Better method of joining to TRANSCRIPTMARKETING

--- Email Notifications/ntfSectionCanceled.sql
+++ Email Notifications/ntfSectionCanceled.sql
@@ -1,14 +1,7 @@
--- ================================================
--- Template generated from Template Explorer using:
--- Create Procedure (New Menu).SQL
---
--- Use the Specify Values for Template Parameters 
--- command (Ctrl-Shift-M) to fill in the parameter 
--- values below.
---
--- This block of comments will not be included in
--- the definition of the procedure.
--- ================================================
+USE [Campus6]
+GO
+
+/****** Object:  StoredProcedure [custom].[ntfSectionCanceled]    Script Date: 2020-04-27 14:26:15 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -22,13 +15,15 @@ GO
 --				Note that message body comes from the Note on the Action Definition for CANCSECT
 --
 -- 2020-01-14 Wyatt Best: Moved code from inside job to stored procedure.
+-- 2020-04-27 Wyatt Best: Realized that TRANSCRIPTMARKETING wasn't matching because REVISION_DATE/TIME are not updated during cancel process!
+--						  Implemented a LIKE match that works much better. False positives are possible but unlikely for MCNY. EVENT_ID pattern is school-specific.
 -- =============================================
 CREATE PROCEDURE [custom].[ntfSectionCanceled]
 AS
 BEGIN
 	SET NOCOUNT ON;
-	SET XACT_ABORT ON; --Stop on all errors.
-	
+	SET XACT_ABORT ON;--Stop on all errors.
+
 	BEGIN TRAN SendEmails
 
 	--Select CANCSECT actions to process into #Messages table
@@ -39,10 +34,10 @@ BEGIN
 		,AC.[ACTION_NAME] [subject]
 		,AC.[NOTE] [body]
 		,CASE 
-			WHEN TM.[EVENT_ID] IS NOT NULL
-				THEN TM.[EVENT_ID] + '/' + CST.[LONG_DESC] + '/' + TM.[SECTION] + ' (' + [EVENT_LONG_NAME] + ')'
+			WHEN oTM.[EVENT_ID] IS NOT NULL
+				THEN oTM.[EVENT_ID] + '/' + CST.[LONG_DESC] + '/' + oTM.[SECTION] + ' (' + [EVENT_LONG_NAME] + ')'
 			WHEN PATINDEX('%20__/%/%/%', ACS.[ACTION_NAME]) > 0
-				THEN SUBSTRING(ACS.[ACTION_NAME], CHARINDEX('/', ACS.[ACTION_NAME], CHARINDEX('/', ACS.[ACTION_NAME], 21) + 1) + 1, 100) + '...' --Try to extract section name from action name (it's often unfortunately truncated)
+				THEN SUBSTRING(ACS.[ACTION_NAME], CHARINDEX('/', ACS.[ACTION_NAME], CHARINDEX('/', ACS.[ACTION_NAME], 21) + 1) + 1, 100) + '...' --Try to extract section name from action name (can be unfortunately truncated)
 			ELSE '(unknown)'
 			END AS [sectionCode]
 		,1 [formatBodyFlag]
@@ -51,28 +46,37 @@ BEGIN
 	FROM [ACTIONSCHEDULE] ACS
 	INNER JOIN [ACTION] AC
 		ON AC.[ACTION_ID] = ACS.[ACTION_ID]
-	LEFT JOIN [TRANSCRIPTMARKETING] TM
-		ON TM.[PEOPLE_CODE_ID] = ACS.[PEOPLE_ORG_CODE_ID]
+	OUTER APPLY (
+		SELECT ACADEMIC_YEAR
+			,ACADEMIC_TERM
+			,ACADEMIC_SESSION
+			,EVENT_ID
+			,EVENT_SUB_TYPE
+			,SECTION
+		FROM TRANSCRIPTMARKETING TM
+		WHERE TM.[PEOPLE_CODE_ID] = ACS.[PEOPLE_ORG_CODE_ID]
+			AND TM.ACADEMIC_YEAR = ACS.ACADEMIC_YEAR
+			AND TM.ACADEMIC_TERM = ACS.ACADEMIC_TERM
+			AND TM.ACADEMIC_SESSION = ACS.ACADEMIC_SESSION
 			AND TM.[DROP_REASON] = 'CANCEL'
-			AND TM.[REVISION_DATE] = ACS.[REQUEST_DATE]
-			AND TM.[REVISION_TIME] BETWEEN (ACS.[REQUEST_TIME] - '00:01:00')
-				AND (ACS.[REQUEST_TIME]) --Allow 60 seconds drift. The cancellation process is slow.
+			AND TM.EVENT_ID = SUBSTRING(ACS.ACTION_NAME, PATINDEX('%/[A-Z][A-Z][A-Z] [0-9][0-9][0-9] [A-Z][A-Z][A-Z]/%', ACS.ACTION_NAME) + 1, 11) --EVENT_ID pattern not applicable to all schools
+			AND TM.SECTION LIKE (SUBSTRING(ACS.ACTION_NAME, LEN(ACS.ACTION_NAME) - CHARINDEX('/', REVERSE(ACS.[ACTION_NAME])) + 2, 50) + '%')
+		) oTM
 	LEFT JOIN [SECTIONS] S
-		ON S.[ACADEMIC_YEAR] = TM.ACADEMIC_YEAR
-			AND S.ACADEMIC_TERM = TM.ACADEMIC_TERM
-			AND S.ACADEMIC_SESSION = TM.ACADEMIC_SESSION
-			AND S.EVENT_ID = TM.EVENT_ID
-			AND S.EVENT_SUB_TYPE = TM.EVENT_SUB_TYPE
-			AND S.SECTION = TM.SECTION
+		ON S.[ACADEMIC_YEAR] = oTM.ACADEMIC_YEAR
+			AND S.ACADEMIC_TERM = oTM.ACADEMIC_TERM
+			AND S.ACADEMIC_SESSION = oTM.ACADEMIC_SESSION
+			AND S.EVENT_ID = oTM.EVENT_ID
+			AND S.EVENT_SUB_TYPE = oTM.EVENT_SUB_TYPE
+			AND S.SECTION = oTM.SECTION
 	LEFT JOIN CODE_EVENTSUBTYPE CST
-		ON CST.CODE_VALUE = TM.EVENT_SUB_TYPE
+		ON CST.CODE_VALUE = oTM.EVENT_SUB_TYPE
 	WHERE ACS.[ACTION_ID] = 'CANCSECT'
 		AND [COMPLETED] <> 'Y'
 		AND [CANCELED] <> 'Y'
 		AND [WAIVED] <> 'Y';
 
 	--SELECT * FROM #Messages; --Debug
-
 	--Supply student names
 	UPDATE #Messages
 	SET [body] = REPLACE([body], '{{FIRST_NAME}}', dbo.fnPeopleOrgName([toId], 'DN'));
@@ -108,7 +112,6 @@ BEGIN
 	EXEC [custom].[spSendEmails];
 
 	--SELECT * FROM #MessagesResults; --Debug
-
 	--Complete Actions upon successful sending
 	UPDATE ACS
 	SET [COMPLETED] = 'Y'
@@ -124,6 +127,3 @@ BEGIN
 
 	COMMIT TRAN SendEmails
 END
-GO
-
-
