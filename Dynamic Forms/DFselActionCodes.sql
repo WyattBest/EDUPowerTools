11842b5bc7e7a8561db7530a2b24635ee4875916
bendodge-xwis <32881391+bendodge-xwis@users.noreply.github.com>
2020-12-08 14:02:05 -0700

    Create DFselActionCodes.sql

---
+++ Dynamic Forms/DFselActionCodes.sql
@@ -0,0 +1,50 @@
+USE [Campus6]
+GO
+
+/****** Object:  StoredProcedure [custom].[DFselAdvisor]    Script Date: 2020-12-08 13:44:39 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		Wyatt Best
+-- Create date: 2020-12-08
+-- Description:	Returns Action definitions for a specific office (or all offices).
+-- =============================================
+ALTER PROCEDURE [custom].[DFselActionCodes] @Office NVARCHAR(10) = NULL
+	,@Type NVARCHAR(6) = NULL
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	--Dynamic Forms has an annoying habit of passing blanks instead of nulls/omitting parameters
+	IF @Office = ''
+		SET @Office = NULL
+
+	IF @Type = ''
+		SET @Type = NULL
+
+	SELECT ACTION_ID
+		,ACTION_NAME
+		,ACTION_NAME + ' (' + ACTION_ID + ')' [NameCode]
+		,ACTION_ID + ' - ' + ACTION_NAME [CodeName]
+		,OFFICE
+		,[TYPE]
+		,NOTE
+	FROM [ACTION]
+	WHERE (
+			OFFICE = @Office
+			OR @Office IS NULL
+			)
+		AND (
+			[TYPE] = @Type
+			OR @Type IS NULL
+			)
+		AND [STATUS] = 'A'
+	ORDER BY ACTION_NAME
+END
+GO
+
+
