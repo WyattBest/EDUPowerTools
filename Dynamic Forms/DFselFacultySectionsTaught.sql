50a5a31cbf392aa92fffa3f13e66b6944f852510
bendodge-xwis <32881391+WyattBest@users.noreply.github.com>
2021-07-12 12:30:01 -0600

    Update DFselFacultySectionsTaught.sql
    Added Faculty Assistants via UNION.

--- Dynamic Forms/DFselFacultySectionsTaught.sql
+++ Dynamic Forms/DFselFacultySectionsTaught.sql
@@ -1,7 +1,7 @@
 USE [Campus6]
 GO
 
-/****** Object:  StoredProcedure [custom].[DFselFacultySectionsTaught]    Script Date: 2021-04-19 15:55:01 ******/
+/****** Object:  StoredProcedure [custom].[DFselFacultySectionsTaught]    Script Date: 2021-07-12 12:29:44 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -14,6 +14,8 @@ GO
 -- Create date: 2021-04-19
 -- Description:	List courses a faculty member has taught.
 --				@PastTerms controls how many past terms are returned. Blank/0 for only current, 1 for current and previous, etc.
+--
+-- 2021-07-12 Wyatt Best:		Added Faculty Assistants via UNION.
 -- =============================================
 CREATE PROCEDURE [custom].[DFselFacultySectionsTaught] @FacultyPCID NVARCHAR(10)
 	,@PastTerms INT = 0
@@ -21,7 +23,7 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 
-	DECLARE @CurTermId INT = [custom].fnGetCurrentTermId()
+	DECLARE @CurTermId INT = [custom].fnGetCurrentTermId();
 
 	SELECT S.SectionId
 		,S.ACADEMIC_YEAR
@@ -33,6 +35,7 @@ BEGIN
 		,S.SECTION
 		,S.EVENT_MED_NAME
 		,S.EVENT_LONG_NAME
+		,TermId
 	FROM SECTIONPER SP
 	INNER JOIN [custom].vwOrderedTerms OT
 		ON OT.ACADEMIC_YEAR = SP.ACADEMIC_YEAR
@@ -45,9 +48,41 @@ BEGIN
 			AND S.EVENT_ID = SP.EVENT_ID
 			AND S.EVENT_SUB_TYPE = SP.EVENT_SUB_TYPE
 			AND S.SECTION = SP.SECTION
-			AND S.EVENT_STATUS IN ('P', 'A')
+			AND S.EVENT_STATUS IN (
+				'P'
+				,'A'
+				)
 	WHERE 1 = 1
 		AND SP.PERSON_CODE_ID = @FacultyPCID
+	
+	UNION ALL
+	
+	SELECT S.SectionId
+		,S.ACADEMIC_YEAR
+		,S.ACADEMIC_TERM
+		,S.ACADEMIC_SESSION
+		,S.ACADEMIC_YEAR + '/' + S.ACADEMIC_TERM + '/' + S.EVENT_ID + '/' + S.EVENT_MED_NAME + '/' + S.SECTION [Year/Term/EventId/Med/Section]
+		,S.ACADEMIC_YEAR + ' / ' + S.ACADEMIC_TERM + ' / ' + S.EVENT_ID + ' / ' + S.EVENT_LONG_NAME + ' / ' + S.SECTION [Year/Term/EventId/Long/Section]
+		,S.EVENT_ID
+		,S.SECTION
+		,S.EVENT_MED_NAME
+		,S.EVENT_LONG_NAME
+		,TermId
+	FROM FacultyAssistant FA
+	INNER JOIN SECTIONS S
+		ON S.SectionId = FA.SectionId
+			AND S.EVENT_STATUS IN (
+				'P'
+				,'A'
+				)
+	INNER JOIN [custom].vwOrderedTerms OT
+		ON OT.ACADEMIC_YEAR = S.ACADEMIC_YEAR
+			AND OT.ACADEMIC_TERM = S.ACADEMIC_TERM
+			AND OT.TermId BETWEEN (@CurTermId - @PastTerms) AND @CurTermId
+	WHERE 1 = 1
+		AND FA.AssistantId = dbo.fnGetPersonId(@FacultyPCID)
+		AND CanAccessOverallGrades = 1
+		AND CanSubmitOverallGrades = 1
 	ORDER BY TermId DESC
 		,EVENT_ID
 		,SECTION
