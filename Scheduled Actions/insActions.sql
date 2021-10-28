63ec986d883bd110babe12b660467bc9ff57bab3
bendodge-xwis <32881391+WyattBest@users.noreply.github.com>
2021-10-28 17:10:32 -0600

    Update insActions.sql
    First working version.

--- Scheduled Actions/insActions.sql
+++ Scheduled Actions/insActions.sql
@@ -15,39 +15,39 @@ GO
 --
 -- Temp table definition
 /*
-		CREATE TABLE #Actions (
-			[ACTION_ID] nvarchar(8) NOT NULL				--Required
-			,[PEOPLE_ORG_CODE_ID] nvarchar(10) NOT NULL	--Required
-			,[SCHEDULED_DATE] datetime NULL
-			,[EXECUTION_DATE] datetime NULL			
-			,[ACTION_NAME] nvarchar(50) NULL			
-			,[RESP_STAFF] nvarchar(10) NULL			
-			,[COMPLETED_BY] nvarchar(10) NULL			
-			,[REQUIRED] nvarchar(1) NOT NULL			
-			,[RATING] nvarchar(3) NULL				
-			,[RESPONSE] nvarchar(6) NULL				
-			,[CONTACT] nvarchar(6) NULL				
-			,[SCHEDULED_TIME] datetime NULL
-			,[NOTE] nvarchar(max) NULL
-			,[COMPLETED] nvarchar(1) NULL
-			,[WAIVED] nvarchar(1) NULL
-			,[WAIVED_REASON] nvarchar(6) NULL
-			,[CANCELED] nvarchar(1) NULL
-			,[CANCELED_REASON] nvarchar(6) NULL
-			,[NUM_OF_REMINDERS] int NULL
-			,[ACADEMIC_YEAR] nvarchar(4) NULL				--Required, can be null
-			,[ACADEMIC_TERM] nvarchar(10) NULL			--Required, can be null
-			,[ACADEMIC_SESSION] nvarchar(10) NULL			--Required, can be null
-			,[DURATION] nvarchar(10) NULL
-			,[DOCUMENT] nvarchar(255) NULL
-			,[Instruction] nvarchar(max) NULL
-			,[OPID] nvarchar(8) NOT NULL					--Required
-			,[ACTIONSCHEDULE_ID] INT NULL
-			)
+	CREATE TABLE #Actions (
+		[ACTION_ID] NVARCHAR(8) NOT NULL			--Required
+		,[PEOPLE_ORG_CODE_ID] NVARCHAR(10) NOT NULL --Required
+		,[SCHEDULED_DATE] DATETIME NULL
+		,[SCHEDULED_TIME] DATETIME NULL
+		,[EXECUTION_DATE] DATETIME NULL
+		,[ACTION_NAME] NVARCHAR(50) NULL
+		,[RESP_STAFF] NVARCHAR(10) NULL
+		,[COMPLETED_BY] NVARCHAR(10) NULL
+		,[REQUIRED] NVARCHAR(1) NOT NULL
+		,[RATING] NVARCHAR(3) NULL
+		,[RESPONSE] NVARCHAR(6) NULL
+		,[CONTACT] NVARCHAR(6) NULL
+		,[NOTE] NVARCHAR(max) NULL
+		,[COMPLETED] NVARCHAR(1) NULL
+		,[WAIVED] NVARCHAR(1) NULL
+		,[WAIVED_REASON] NVARCHAR(6) NULL
+		,[CANCELED] NVARCHAR(1) NULL
+		,[CANCELED_REASON] NVARCHAR(6) NULL
+		,[NUM_OF_REMINDERS] INT NULL
+		,[ACADEMIC_YEAR] NVARCHAR(4) NULL
+		,[ACADEMIC_TERM] NVARCHAR(10) NULL
+		,[ACADEMIC_SESSION] NVARCHAR(10) NULL
+		,[DURATION] NVARCHAR(10) NULL
+		,[DOCUMENT] NVARCHAR(255) NULL
+		,[Instruction] NVARCHAR(max) NULL
+		,[OPID] NVARCHAR(8) NOT NULL				--Required
+		,[ACTIONSCHEDULE_ID] INT NULL
+		)
 */
 --
 -- =============================================
-CREATE PROCEDURE [custom].insAction
+ALTER PROCEDURE [custom].insActions
 AS
 BEGIN
 	SET NOCOUNT ON;
@@ -58,45 +58,48 @@ BEGIN
 			FROM tempdb.sys.columns
 			WHERE [object_id] = OBJECT_ID('tempdb..#Actions')
 			)
+	BEGIN
 		RAISERROR (
 				'#Actions temp table does not exist.'
 				,11
 				,1
-				);
+				)
+
+		RETURN
+	END
 
 	IF 'ACTION_ID' NOT IN (
 			SELECT [name]
 			FROM tempdb.sys.columns
 			WHERE [object_id] = OBJECT_ID('tempdb..#Actions')
 			)
+	BEGIN
 		RAISERROR (
 				'Column ACTION_ID does not exist in #Actions.'
 				,11
 				,1
-				);
+				)
+
+		RETURN
+	END
 
 	IF 'PEOPLE_ORG_CODE_ID' NOT IN (
 			SELECT [name]
 			FROM tempdb.sys.columns
 			WHERE [object_id] = OBJECT_ID('tempdb..#Actions')
 			)
+	BEGIN
 		RAISERROR (
 				'Column PEOPLE_ORG_CODE_ID does not exist in #Actions.'
 				,11
 				,1
-				);
+				)
+
+		RETURN
+	END
 
 	DECLARE @Today DATETIME = dbo.fnmakeDate(getdate())
 		,@Now DATETIME = dbo.fnmakeTime(getdate());
-
-	--Add a primary key to #Actions
-	ALTER TABLE #Actions ADD [Key] INT IDENTITY PRIMARY KEY;
-
-	--Declare a table to hold new ACTIONSCHEDULE_ID's
-	DECLARE @NewActionIds TABLE (
-		[Key] INT NOT NULL
-		,[ACTIONSCHEDULE_ID] INT NOT NULL
-		);
 
 	--Add optional columns to #Actions
 	IF NOT EXISTS (
@@ -106,6 +109,14 @@ BEGIN
 				AND [name] = 'SCHEDULED_DATE'
 			)
 		ALTER TABLE #Actions ADD [SCHEDULED_DATE] DATETIME NULL;
+
+	IF NOT EXISTS (
+			SELECT *
+			FROM tempdb.sys.columns
+			WHERE [object_id] = OBJECT_ID('tempdb..#Actions')
+				AND [name] = 'SCHEDULED_TIME'
+			)
+		ALTER TABLE #Actions ADD [SCHEDULED_TIME] DATETIME NULL;
 
 	IF NOT EXISTS (
 			SELECT *
@@ -145,7 +156,7 @@ BEGIN
 			WHERE [object_id] = OBJECT_ID('tempdb..#Actions')
 				AND [name] = 'REQUIRED'
 			)
-		ALTER TABLE #Actions ADD [REQUIRED] NVARCHAR(1) NOT NULL;
+		ALTER TABLE #Actions ADD [REQUIRED] NVARCHAR(1) NULL;
 
 	IF NOT EXISTS (
 			SELECT *
@@ -167,9 +178,49 @@ BEGIN
 			SELECT *
 			FROM tempdb.sys.columns
 			WHERE [object_id] = OBJECT_ID('tempdb..#Actions')
+				AND [name] = 'CONTACT'
+			)
+		ALTER TABLE #Actions ADD [CONTACT] NVARCHAR(6) NULL;
+
+	IF NOT EXISTS (
+			SELECT *
+			FROM tempdb.sys.columns
+			WHERE [object_id] = OBJECT_ID('tempdb..#Actions')
 				AND [name] = 'NOTE'
 			)
 		ALTER TABLE #Actions ADD [NOTE] NVARCHAR(max) NULL;
+
+	IF NOT EXISTS (
+			SELECT *
+			FROM tempdb.sys.columns
+			WHERE [object_id] = OBJECT_ID('tempdb..#Actions')
+				AND [name] = 'COMPLETED'
+			)
+		ALTER TABLE #Actions ADD [COMPLETED] NVARCHAR(1) NULL;
+
+	IF NOT EXISTS (
+			SELECT *
+			FROM tempdb.sys.columns
+			WHERE [object_id] = OBJECT_ID('tempdb..#Actions')
+				AND [name] = 'WAVIED'
+			)
+		ALTER TABLE #Actions ADD [WAIVED] NVARCHAR(1) NULL;
+
+	IF NOT EXISTS (
+			SELECT *
+			FROM tempdb.sys.columns
+			WHERE [object_id] = OBJECT_ID('tempdb..#Actions')
+				AND [name] = 'WAIVED_REASON'
+			)
+		ALTER TABLE #Actions ADD [WAIVED_REASON] NVARCHAR(6) NULL;
+
+	IF NOT EXISTS (
+			SELECT *
+			FROM tempdb.sys.columns
+			WHERE [object_id] = OBJECT_ID('tempdb..#Actions')
+				AND [name] = 'CANCELED'
+			)
+		ALTER TABLE #Actions ADD [CANCELED] NVARCHAR(1) NULL;
 
 	IF NOT EXISTS (
 			SELECT *
@@ -186,6 +237,30 @@ BEGIN
 				AND [name] = 'NUM_OF_REMINDERS'
 			)
 		ALTER TABLE #Actions ADD [NUM_OF_REMINDERS] INT NULL;
+
+	IF NOT EXISTS (
+			SELECT *
+			FROM tempdb.sys.columns
+			WHERE [object_id] = OBJECT_ID('tempdb..#Actions')
+				AND [name] = 'ACADEMIC_YEAR'
+			)
+		ALTER TABLE #Actions ADD [ACADEMIC_YEAR] NVARCHAR(4) NULL;
+
+	IF NOT EXISTS (
+			SELECT *
+			FROM tempdb.sys.columns
+			WHERE [object_id] = OBJECT_ID('tempdb..#Actions')
+				AND [name] = 'ACADEMIC_TERM'
+			)
+		ALTER TABLE #Actions ADD [ACADEMIC_TERM] NVARCHAR(10) NULL;
+
+	IF NOT EXISTS (
+			SELECT *
+			FROM tempdb.sys.columns
+			WHERE [object_id] = OBJECT_ID('tempdb..#Actions')
+				AND [name] = 'ACADEMIC_SESSION'
+			)
+		ALTER TABLE #Actions ADD [ACADEMIC_SESSION] NVARCHAR(10) NULL;
 
 	IF NOT EXISTS (
 			SELECT *
@@ -219,109 +294,185 @@ BEGIN
 			)
 		ALTER TABLE #Actions ADD [ACTIONSCHEDULE_ID] INT NULL;
 
-	--INSERT into ACTIONSCHEDULE and capture the new ID's.
-	INSERT INTO [ACTIONSCHEDULE] (
-		[ACTION_ID]
-		,[PEOPLE_ORG_CODE]
-		,[PEOPLE_ORG_ID]
-		,[PEOPLE_ORG_CODE_ID]
-		,[REQUEST_DATE]
-		,[REQUEST_TIME]
-		,[SCHEDULED_DATE]
-		,[EXECUTION_DATE]
-		,[CREATE_DATE]
-		,[CREATE_TIME]
-		,[CREATE_OPID]
-		,[CREATE_TERMINAL]
-		,[REVISION_DATE]
-		,[REVISION_TIME]
-		,[REVISION_OPID]
-		,[REVISION_TERMINAL]
-		,[ABT_JOIN]
-		,[ACTION_NAME]
-		,[OFFICE]
-		,[TYPE]
-		,[RESP_STAFF]
-		,[COMPLETED_BY]
-		,[REQUIRED]
-		,[PRIORITY]
-		,[RATING]
-		,[RESPONSE]
-		,[CONTACT]
-		,[SCHEDULED_TIME]
-		,[NOTE]
-		,[UNIQUE_KEY]
-		,[COMPLETED]
-		,[WAIVED]
-		,[WAIVED_REASON]
-		,[CANCELED]
-		,[CANCELED_REASON]
-		,[NUM_OF_REMINDERS]
-		,[ACADEMIC_YEAR]
-		,[ACADEMIC_TERM]
-		,[ACADEMIC_SESSION]
-		,[RULE_ID]
-		,[SEQ_NUM]
-		,[DURATION]
-		,[DOCUMENT]
-		,[Instruction]
-		)
-	OUTPUT INSERTED.ACTIONSCHEDULE_ID
-	INTO @NewActionIds
-	SELECT A.[ACTION_ID]
-		,LEFT(PEOPLE_ORG_CODE_ID, 1) AS [PEOPLE_ORG_CODE]
-		,RIGHT(PEOPLE_ORG_CODE_ID, 9) AS [PEOPLE_ORG_ID]
-		,[PEOPLE_ORG_CODE_ID]
-		,@Today AS [REQUEST_DATE]
-		,@Now AS [REQUEST_TIME]
-		,[SCHEDULED_DATE]
-		,[EXECUTION_DATE]
-		,@Today AS [CREATE_DATE]
-		,@Now AS [CREATE_TIME]
-		,A.OPID AS [CREATE_OPID]
-		,'0001' AS [CREATE_TERMINAL]
-		,@Today AS [REVISION_DATE]
-		,@Now AS [REVISION_TIME]
-		,OPID AS [REVISION_OPID]
-		,'0001' AS [REVISION_TERMINAL]
-		,'*' AS [ABT_JOIN]
-		,COALESCE(ACTION_NAME, CA.ACTION_NAME) AS [ACTION_NAME]
-		,CA.[OFFICE]
-		,CA.[TYPE]
-		,A.[RESP_STAFF]
-		,[COMPLETED_BY]
-		,COALESCE(A.[REQUIRED], CA.REQUIRED) AS [REQUIRED]
-		,CA.[PRIORITY]
-		,COALESCE(A.[RATING], CA.RATING) AS [RATING]
-		,COALESCE(A.[RESPONSE], CA.RESPONSE) AS [RESPONSE]
-		,[CONTACT]
-		,COALESCE(@Now, A.SCHEDULED_TIME) AS [SCHEDULED_TIME]
-		,[NOTE]
-		,A.ACTION_ID + convert(NVARCHAR(4), datepart(yy, GETDATE())) + convert(NVARCHAR(2), datepart(mm, GETDATE())) + convert(NVARCHAR(2), datepart(dd, GETDATE())) + convert(NVARCHAR(2), datepart(hh, GETDATE())) + convert(NVARCHAR(2), datepart(mi, GETDATE())) + convert(NVARCHAR(4), datepart(ms, GETDATE())) AS [UNIQUE_KEY]
-		,[COMPLETED]
-		,[WAIVED]
-		,[WAIVED_REASON]
-		,[CANCELED]
-		,[CANCELED_REASON]
-		,[NUM_OF_REMINDERS]
-		,[ACADEMIC_YEAR]
-		,[ACADEMIC_TERM]
-		,[ACADEMIC_SESSION]
-		,0 AS [RULE_ID]
-		,0 AS [SEQ_NUM]
-		,[DURATION]
-		,[DOCUMENT]
-		,COALESCE(A.INSTRUCTION, CA.INSTRUCTION) AS [Instruction]
-	FROM #Actions A
-	INNER JOIN [ACTION] CA
-		ON CA.[ACTION_ID] = A.[ACTION_ID]
+	--Add a primary key to #Actions
+	ALTER TABLE #Actions ADD [Key] INT IDENTITY PRIMARY KEY;
+
+	--Create a table to hold new ACTIONSCHEDULE_ID's
+	CREATE TABLE #NewActionIds (
+		[Key] INT NOT NULL
+		,[ACTIONSCHEDULE_ID] INT NOT NULL
+		);
+
+	--A stupid step to prevent compiler from erroring because it doesn't recognize the new columns as existing.
+	--Might be able to remove in a future version of SQL Server.
+	SELECT *
+	INTO #ActionsIntermediate
+	FROM #Actions;
+
+	--Debug
+	SELECT *
+	FROM #ActionsIntermediate;
+
+	--Insert into ACTIONSCHEDULE. A MERGE statement is used to allow returning columns we didn't insert, specifically #Actions.Key.
+	MERGE INTO ACTIONSCHEDULE AS T
+	USING (
+		SELECT A.[ACTION_ID]
+			,LEFT(PEOPLE_ORG_CODE_ID, 1) AS [PEOPLE_ORG_CODE]
+			,RIGHT(PEOPLE_ORG_CODE_ID, 9) AS [PEOPLE_ORG_ID]
+			,[PEOPLE_ORG_CODE_ID]
+			,@Today AS [REQUEST_DATE]
+			,@Now AS [REQUEST_TIME]
+			,[SCHEDULED_DATE]
+			,[EXECUTION_DATE]
+			,@Today AS [CREATE_DATE]
+			,@Now AS [CREATE_TIME]
+			,A.OPID AS [CREATE_OPID]
+			,'0001' AS [CREATE_TERMINAL]
+			,@Today AS [REVISION_DATE]
+			,@Now AS [REVISION_TIME]
+			,OPID AS [REVISION_OPID]
+			,'0001' AS [REVISION_TERMINAL]
+			,'*' AS [ABT_JOIN]
+			,COALESCE(A.ACTION_NAME, CA.ACTION_NAME) AS [ACTION_NAME]
+			,CA.[OFFICE]
+			,CA.[TYPE]
+			,A.[RESP_STAFF]
+			,[COMPLETED_BY]
+			,COALESCE(A.[REQUIRED], CA.REQUIRED) AS [REQUIRED]
+			,CA.[PRIORITY]
+			,[RATING]
+			,[RESPONSE]
+			,[CONTACT]
+			,COALESCE(@Now, A.SCHEDULED_TIME) AS [SCHEDULED_TIME]
+			,A.[NOTE]
+			,A.ACTION_ID + convert(NVARCHAR(4), datepart(yy, GETDATE())) + convert(NVARCHAR(2), datepart(mm, GETDATE())) + convert(NVARCHAR(2), datepart(dd, GETDATE())) + convert(NVARCHAR(2), datepart(hh, GETDATE())) + convert(NVARCHAR(2), datepart(mi, GETDATE())) + convert(NVARCHAR(4), datepart(ms, GETDATE())) AS [UNIQUE_KEY]
+			,COALESCE([COMPLETED], 'N') AS [COMPLETED]
+			,COALESCE([WAIVED], 'N') AS [WAIVED]
+			,[WAIVED_REASON]
+			,COALESCE([CANCELED], 'N') AS [CANCELED]
+			,[CANCELED_REASON]
+			,COALESCE([NUM_OF_REMINDERS], 0) AS [NUM_OF_REMINDERS]
+			,[ACADEMIC_YEAR]
+			,[ACADEMIC_TERM]
+			,[ACADEMIC_SESSION]
+			,0 AS [RULE_ID]
+			,0 AS [SEQ_NUM]
+			,[DURATION]
+			,[DOCUMENT]
+			,COALESCE(A.[Instruction], CA.[Instruction]) AS [Instruction]
+			,[Key]
+		FROM #ActionsIntermediate A
+		INNER JOIN [ACTION] CA
+			ON CA.ACTION_ID = A.ACTION_ID
+		) AS A
+		ON 1 = 0 --Will never be true, so no UPDATEs will ever happen
+	WHEN NOT MATCHED
+		THEN
+			INSERT (
+				[ACTION_ID]
+				,[PEOPLE_ORG_CODE]
+				,[PEOPLE_ORG_ID]
+				,[PEOPLE_ORG_CODE_ID]
+				,[REQUEST_DATE]
+				,[REQUEST_TIME]
+				,[SCHEDULED_DATE]
+				,[EXECUTION_DATE]
+				,[CREATE_DATE]
+				,[CREATE_TIME]
+				,[CREATE_OPID]
+				,[CREATE_TERMINAL]
+				,[REVISION_DATE]
+				,[REVISION_TIME]
+				,[REVISION_OPID]
+				,[REVISION_TERMINAL]
+				,[ABT_JOIN]
+				,[ACTION_NAME]
+				,[OFFICE]
+				,[TYPE]
+				,[RESP_STAFF]
+				,[COMPLETED_BY]
+				,[REQUIRED]
+				,[PRIORITY]
+				,[RATING]
+				,[RESPONSE]
+				,[CONTACT]
+				,[SCHEDULED_TIME]
+				,[NOTE]
+				,[UNIQUE_KEY]
+				,[COMPLETED]
+				,[WAIVED]
+				,[WAIVED_REASON]
+				,[CANCELED]
+				,[CANCELED_REASON]
+				,[NUM_OF_REMINDERS]
+				,[ACADEMIC_YEAR]
+				,[ACADEMIC_TERM]
+				,[ACADEMIC_SESSION]
+				,[RULE_ID]
+				,[SEQ_NUM]
+				,[DURATION]
+				,[DOCUMENT]
+				,[Instruction]
+				)
+			VALUES (
+				[ACTION_ID]
+				,[PEOPLE_ORG_CODE]
+				,[PEOPLE_ORG_ID]
+				,[PEOPLE_ORG_CODE_ID]
+				,[REQUEST_DATE]
+				,[REQUEST_TIME]
+				,[SCHEDULED_DATE]
+				,[EXECUTION_DATE]
+				,[CREATE_DATE]
+				,[CREATE_TIME]
+				,[CREATE_OPID]
+				,[CREATE_TERMINAL]
+				,[REVISION_DATE]
+				,[REVISION_TIME]
+				,[REVISION_OPID]
+				,[REVISION_TERMINAL]
+				,[ABT_JOIN]
+				,[ACTION_NAME]
+				,[OFFICE]
+				,[TYPE]
+				,[RESP_STAFF]
+				,[COMPLETED_BY]
+				,[REQUIRED]
+				,[PRIORITY]
+				,[RATING]
+				,[RESPONSE]
+				,[CONTACT]
+				,[SCHEDULED_TIME]
+				,[NOTE]
+				,[UNIQUE_KEY]
+				,[COMPLETED]
+				,[WAIVED]
+				,[WAIVED_REASON]
+				,[CANCELED]
+				,[CANCELED_REASON]
+				,[NUM_OF_REMINDERS]
+				,[ACADEMIC_YEAR]
+				,[ACADEMIC_TERM]
+				,[ACADEMIC_SESSION]
+				,[RULE_ID]
+				,[SEQ_NUM]
+				,[DURATION]
+				,[DOCUMENT]
+				,[Instruction]
+				)
+	OUTPUT inserted.ACTIONSCHEDULE_ID
+		,A.[Key]
+	INTO #NewActionIds;
 
 	--Update #Actions with the new ACTIONSCHEDULE_ID's
-	UPDATE A
+	--Using EXEC because the compiler doens't know that [Key] exists
+	EXEC (
+			'UPDATE A
 	SET [ACTIONSCHEDULE_ID] = B.[ActionSchedule_Id]
 	FROM #Actions A
-	INNER JOIN @NewActionIds B
-		ON A.[Key] = B.[Key]
+	INNER JOIN #NewActionIds B
+		ON A.[Key] = B.[Key]'
+			)
 END
 GO
 
